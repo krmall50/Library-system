9b35d3109a5f6bb9aae6e01053c9c62f86cbfcc9
aiswlz <230787@astanait.edu.kz>
2024-02-20 23:06:59 +0600

    Added Borrower class.

---
+++ src/Borrower.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Borrower extends Person {
+
+    private ArrayList<HoldRequest> holdRequests;
+
+    public Borrower(int id, String name, String address, int phoneNo) {
+        super(id, name, address, phoneNo);
+        this.holdRequests = new ArrayList<>();
+    }
+
+    public void addHoldRequest(HoldRequest request) {
+        holdRequests.add(request);
+    }
+
+    public void removeHoldRequest(HoldRequest request) {
+        holdRequests.remove(request);
+    }
+
+    public ArrayList<HoldRequest> getHoldRequests() {
+        return holdRequests;
+    }
+
+    @Override
+    public String toString() {
+        return "Borrower [id=" + id + ", name=" + name + ", address=" + address
+                + ", phoneNo=" + phoneNo + ", holdRequests=" + holdRequests + "]";
+    }
+}
