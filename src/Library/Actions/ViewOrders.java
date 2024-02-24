1d4b7c7e4e6d247985d18e0c2bce466b5320ef06
solta <230930@astanait.edu.kz>
2024-02-24 17:06:50 +0600

    added ViewOrders

--- src/Library/Actions/ViewOrders.java
+++ src/Library/Actions/ViewOrders.java
@@ -7,6 +7,21 @@ import Library.Users.User;
 public class ViewOrders implements IOOperation {
     @Override
     public void oper(Database database, User user) {
+        System.out.println("\nEnter book name:");
+        String bookname = s.next();
+        int i = database.getBook(bookname);
+        if (i>-1){
+            System.out.println("Book\t\tUser\t\tQty\tPrice");
+            for (Order order : database.getAllorders()){
+                if(order.getBook().getName().matches(bookname)){
+                    System.out.println(order.getBook().getName()+"\t\t"+ order.getUser().getName()+"\t\t"+ order.getQty()+"\t"+order.getPrice());
+                }
+                System.out.println();
+            }
+        }else {
+            System.out.println("Book doesn't exist!\n");
+        }
+        user.menu(database, user);
 
     }
 }
